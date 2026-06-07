a239f5e44cbb2eb4982b08c561442270463d94a3
NabilaAinnurRohmah <ainnurnabila577@gmail.com>
2026-06-07 10:33:20 +0700

    Update

--- app/Http/Controllers/XorCipherController.php
+++ app/Http/Controllers/XorCipherController.php
@@ -6,35 +6,64 @@ use Illuminate\Http\Request;
 
 class XorCipherController extends Controller
 {
-
     private $ascii = [
 
-        'A'=>65,'B'=>66,'C'=>67,'D'=>68,'E'=>69,
-        'F'=>70,'G'=>71,'H'=>72,'I'=>73,'J'=>74,
-        'K'=>75,'L'=>76,'M'=>77,'N'=>78,'O'=>79,
-        'P'=>80,'Q'=>81,'R'=>82,'S'=>83,'T'=>84,
-        'U'=>85,'V'=>86,'W'=>87,'X'=>88,'Y'=>89,
-        'Z'=>90,
+        'A' => 65, 'B' => 66, 'C' => 67, 'D' => 68, 'E' => 69,
+        'F' => 70, 'G' => 71, 'H' => 72, 'I' => 73, 'J' => 74,
+        'K' => 75, 'L' => 76, 'M' => 77, 'N' => 78, 'O' => 79,
+        'P' => 80, 'Q' => 81, 'R' => 82, 'S' => 83, 'T' => 84,
+        'U' => 85, 'V' => 86, 'W' => 87, 'X' => 88, 'Y' => 89,
+        'Z' => 90,
 
-        'a'=>97,'b'=>98,'c'=>99,'d'=>100,'e'=>101,
-        'f'=>102,'g'=>103,'h'=>104,'i'=>105,'j'=>106,
-        'k'=>107,'l'=>108,'m'=>109,'n'=>110,'o'=>111,
-        'p'=>112,'q'=>113,'r'=>114,'s'=>115,'t'=>116,
-        'u'=>117,'v'=>118,'w'=>119,'x'=>120,'y'=>121,
-        'z'=>122,
+        'a' => 97, 'b' => 98, 'c' => 99, 'd' => 100, 'e' => 101,
+        'f' => 102, 'g' => 103, 'h' => 104, 'i' => 105, 'j' => 106,
+        'k' => 107, 'l' => 108, 'm' => 109, 'n' => 110, 'o' => 111,
+        'p' => 112, 'q' => 113, 'r' => 114, 's' => 115, 't' => 116,
+        'u' => 117, 'v' => 118, 'w' => 119, 'x' => 120, 'y' => 121,
+        'z' => 122,
 
-        '0'=>48,'1'=>49,'2'=>50,'3'=>51,'4'=>52,
-        '5'=>53,'6'=>54,'7'=>55,'8'=>56,'9'=>57,
+        '0' => 48, '1' => 49, '2' => 50, '3' => 51, '4' => 52,
+        '5' => 53, '6' => 54, '7' => 55, '8' => 56, '9' => 57,
 
-        ' '=>32
+        ' ' => 32,
+        '.' => 46,
+        ',' => 44,
+        '-' => 45,
+        '_' => 95,
+        '/' => 47,
+        '@' => 64,
+        '!' => 33,
+        '#' => 35,
+        ':' => 58,
+        ';' => 59,
+        '?' => 63,
+        '=' => 61,
+        '+' => 43,
+        '*' => 42,
+        '%' => 37,
+        '$' => 36,
+        '&' => 38,
+        '(' => 40,
+        ')' => 41,
+        '[' => 91,
+        ']' => 93,
+        '{' => 123,
+        '}' => 125,
+        '<' => 60,
+        '>' => 62,
+        '"' => 34,
+        "'" => 39,
+        '\\' => 92,
+        '|' => 124,
+        '^' => 94,
+        '~' => 126,
+        '`' => 96,
     ];
-
 
     private function charToAscii($char)
     {
         return $this->ascii[$char] ?? 0;
     }
-
 
     private function asciiToChar($ascii)
     {
@@ -48,25 +77,23 @@ class XorCipherController extends Controller
         return '-';
     }
 
-
     private function decimalToBinary($number)
     {
         $binary = '';
 
         while ($number > 0) {
 
-            $binary = ($number % 2) . $binary;
+            $binary = ($number % 2).$binary;
 
-            $number = (int)($number / 2);
+            $number = (int) ($number / 2);
         }
 
         while (strlen($binary) < 8) {
-            $binary = '0' . $binary;
+            $binary = '0'.$binary;
         }
 
         return $binary;
     }
-
 
     private function binaryToDecimal($binary)
     {
@@ -74,7 +101,7 @@ class XorCipherController extends Controller
 
         $power = 0;
 
-        for ($i = strlen($binary)-1; $i >= 0; $i--) {
+        for ($i = strlen($binary) - 1; $i >= 0; $i--) {
 
             if ($binary[$i] == '1') {
 
@@ -86,7 +113,6 @@ class XorCipherController extends Controller
 
         return $decimal;
     }
-
 
     private function textToBinary($text)
     {
@@ -108,7 +134,6 @@ class XorCipherController extends Controller
         return $result;
     }
 
-
     private function xorBinary($bin1, $bin2)
     {
         $hasil = '';
@@ -124,7 +149,6 @@ class XorCipherController extends Controller
 
         return $hasil;
     }
-
 
     private function isBinaryInput($text)
     {
@@ -142,12 +166,10 @@ class XorCipherController extends Controller
         return true;
     }
 
-
     public function index()
     {
         return view('xor');
     }
-
 
     public function process(Request $request)
     {
@@ -171,7 +193,6 @@ class XorCipherController extends Controller
             );
         }
 
-
         $keyBinary = $this->textToBinary(
             $key
         );
@@ -188,7 +209,6 @@ class XorCipherController extends Controller
                 $i % count($keyBinary)
             ];
 
-
             $xor = $this->xorBinary(
                 $binInput,
                 $binKey
@@ -199,7 +219,6 @@ class XorCipherController extends Controller
             $decimal = $this->binaryToDecimal(
                 $xor
             );
-
 
             $resultText .= $this->asciiToChar(
                 $decimal
@@ -219,7 +238,7 @@ class XorCipherController extends Controller
                 $resultBinary
             ),
 
-            'resultText' => $resultText
+            'resultText' => $resultText,
         ]);
     }
 }
